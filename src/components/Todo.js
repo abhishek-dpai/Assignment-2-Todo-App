f9e64e7e0a89065ab84f3de7893e12ae8e440a32
Shubham Meshram <oneshubh@gmail.com>
2020-09-26 13:52:56 +0530

    [CODE : SUGGESTIONS]

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,17 +5,17 @@ const Todo = ({ id, text, todo, todos, setTodos }) => {
         setTodos(todos.filter((el) => el.id !== todo.id))
     }
     const completeHandler = () => {
+        //check whether you can do this with Map()
+        const completedTodos = todos.map((item) => {
+            if (item.id === todo.id) {
+                return {
+                    ...item, completed: !item.completed
+                };
+            }
+            return item
+        }) // intermediate result helps in debugging, if you computing the result in param will cause problems while debugging.
         setTodos(
-            todos.map((item) => {
-                if (item.id === todo.id) {
-                    return {
-                        ...item, completed: !item.completed
-                    };
-                }
-
-                return item
-
-            })
+            completedTodos
         );
     }
 
