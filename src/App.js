e6e1679d92bf2a66d85adb906e9587a92591ab77
raiab <abhishekr@dailyprophet.ai>
2020-10-01 16:35:27 +0530

    first commit to new-popup

--- src/App.js
+++ src/App.js
@@ -1,80 +1,66 @@
-  import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 //importing components
-import Form from "./components/Form"
-import TodoList from "./components/TodoList"
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 function App() {
   //state
   const [inputText, setInputText] = useState(" ");
-  const [description, setDescription] = useState(" ");
-  const [date, setDate] = useState(" ");
-  const [priority, setPriority] = useState(" ");
   const [todos, setTodos] = useState([]);
-  const [todosDetails, setTodosDetails] = useState([]);
   const [status, setStatus] = useState("all");
-  // status is the filter for all todo, competed or not, sorted by priority 
+  // status is the filter for all todo, competed or not, sorted by priority
   const [filteredTodos, setFilteredTodos] = useState([]);
   useEffect(() => {
     getLocalTodos();
-  }, [])
+  }, []);
   useEffect(() => {
     filterHandler();
     saveLocalTodos();
-  }, [todos, status])
+  }, [todos, status]);
   //functions
 
   const filterHandler = () => {
     switch (status) {
       case "completed":
-        setFilteredTodos(todos.filter((todo) => todo.completed === true))
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
         break;
       case "uncompleted":
-        setFilteredTodos(todos.filter((todo) => todo.completed === false))
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
         break;
       default:
         setFilteredTodos(todos);
         break;
     }
-  }
+  };
   //save to local
   const saveLocalTodos = () => {
-    localStorage.setItem("todos", JSON.stringify(todos))
-
-  }
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
   const getLocalTodos = () => {
-
     if (localStorage.getItem("todos") === null) {
       localStorage.setItem("todos", JSON.stringify([]));
-    }
-    else {
-      let todoLocal = JSON.parse(localStorage.getItem("todos"))
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem("todos"));
       setTodos(todoLocal);
-
     }
-  }
+  };
   return (
     <div className="App">
       <header>
         <h1> Abhishek's Todo App </h1>
       </header>
-      <Form 
-        description={description}
-        setDescription={setDescription}
-        date={date}
-        setDate={setDate}
-        priority={priority}
-        setPriority={setPriority}
+      <Form
         setStatus={setStatus}
-        todosDetails={todosDetails}
-        setTodosDetails={setTodosDetails} 
-        todos={todos} 
-        setTodos={setTodos} 
-        inputText={inputText} 
-        setInputText={setInputText} />
-      <TodoList 
-        filteredTodos={filteredTodos} 
-        todos={todos} 
-        setTodos={setTodos} />
+        todos={todos}
+        setTodos={setTodos}
+        inputText={inputText}
+        setInputText={setInputText}
+      />
+      <TodoList
+        filteredTodos={filteredTodos}
+        todos={todos}
+        setTodos={setTodos}
+      />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,53 +1,31 @@
-import React,{useState} from "react";
-import PopUpForm from "./PopUpForm";
-const Form = ({ priority, setPriority,  date, setDate, description, setDescription,inputText, setInputText, todosDetails, 
-              setTodosDetails, todos, setTodos ,setStatus}) => {
+import React, { useState } from "react";
+const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
   //props
-  const newId=Math.floor(Math.random() * 1000000); // there might be collision
-  const [clickedCounter,setClickedCounter]=useState(0);
+  const newId = Math.floor(Math.random() * 1000000); // there might be collision
   const inputTextHandler = (e) => {
     setInputText(e.target.value);
   };
   const submitTodoHandler = (e) => {
-    console.log("newId="+newId);
-    console.log("clickedCounter= "+clickedCounter);
+    console.log("newId=" + newId);
     e.preventDefault(); // stops browser from refreshing
-    setTodos([...todos,
-    { text: inputText, completed: false, id: newId }
-    ]);
-    setClickedCounter(clickedCounter+1);
+    setTodos([...todos, { text: inputText, completed: false, id: newId }]);
     setInputText("");
   };
-  const statusHandler=(e)=>{
-    setStatus(e.target.value)
-  }
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
   return (
     <form>
-      <input 
-        value={inputText} 
-        onChange={inputTextHandler} 
-        type="text" 
-        className="todo-input" />
-      <button 
-        onClick={submitTodoHandler}
-        className="todo-button" 
-        type="submit"> 
-           <i className="fas fa-plus-square"></i>
+      <input
+        value={inputText}
+        onChange={inputTextHandler}
+        type="text"
+        className="todo-input"
+      />
+      <button onClick={submitTodoHandler} className="todo-button" type="submit">
+        <i className="fas fa-plus-square"></i>
       </button>
-      {
-        clickedCounter!==0?<PopUpForm 
-                                    description={description}
-                                    setDescription={setDescription}
-                                    date={date}
-                                    setDate={setDate}
-                                    priority={priority}
-                                    setPriority={setPriority}
-                                    todosDetails={todosDetails} 
-                                    setTodosDetails={setTodosDetails}
-                                     id={newId} />: "" 
-    
-      }
-      
+
       <div className="select">
         <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
