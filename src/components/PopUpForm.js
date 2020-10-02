0b47d263ab52d70691b9535dc4b048fc971d3071
raiab <abhishekr@dailyprophet.ai>
2020-10-02 17:36:08 +0530

    first popup-added  commit

--- src/components/PopUpForm.js
+++ src/components/PopUpForm.js
@@ -1,42 +1,62 @@
-import React from "react";
-import '../App.css';
+import React, { Component } from "react";
 
-const PopUpForm = (props) => {
-    const {priority, setPriority, date, setDate, description, setDescription,todosDetails, setTodosDetails, id}=props;
-    const inputDescriptionHandler=(e)=>{
-        setDescription(e.target.value);
-    }
-    const inputDateHandler=(e)=>{
-        setDate(e.target.value);
-    }
-    const inputPriorityHandler=(e)=>{
-        setPriority(e.target.value);
-    }
-    const submitDetailsHandler=(e)=>{
-        e.preventDefault();
-        console.log(todosDetails);
-         setTodosDetails([...todosDetails, {description: description, date:date, priority:priority  }]);
-    }
-    
+class PopUpForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      description: "",
+      date: "",
+      priority: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
     return (
+      <main className="todo-popup-container">
+        <form className="popup-form">
+          <input
+            name="description"
+            value={this.state.description}
+            onChange={this.handleChange}
+            placeholder="Description"
+          />
 
-        <div className="model-bg-popup">
-            <div className="model-popup">
-                <h2>Pop-up form</h2>
-                <label htmlFor="description">Description:</label>
-                <input onChange={inputDescriptionHandler}  type="text" name="description"/>
-                <br/>
-                <label htmlFor="date">Date:</label>
-                <input onChange={inputDateHandler} type="text" name="date"/>
-                <br/>
-                <label htmlFor="priority">Priority:</label>
-                <input onChange={inputPriorityHandler} type="text" name="priority"/>
-                <br/>
-                <button onClick={submitDetailsHandler}>Submit</button>
-                <span className="modal-close">X</span>
-            </div>
+          <input
+            name="date"
+            value={this.state.date}
+            onChange={this.handleChange}
+            placeholder="Date"
+          />
 
-        </div>    
-    )
-};
+          <select
+            value={this.state.priority}
+            name="priority"
+            onChange={this.handleChange}
+          >
+            <option value="">-- Please Choose a Priority --</option>
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+          </select>
+
+          <button>Submit</button>
+        </form>
+        <hr />
+        <h2>Entered information:</h2>
+        <p>Entered Description: {this.state.description}</p>
+        <p>Entered Date: {this.state.date}</p>
+        <p>Choosed Priority: {this.state.priority}</p>
+      </main>
+    );
+  }
+}
+
 export default PopUpForm;
