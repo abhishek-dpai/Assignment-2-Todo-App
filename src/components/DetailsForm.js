ab378053a56761676161d3c90d630b7bb260c303
Shubham Meshram <oneshubh@gmail.com>
2020-10-03 19:36:11 +0530

    [DETAILS-FORM : CORRECTIONS] details form rendering and usage fixed.

--- src/components/DetailsForm.js
+++ src/components/DetailsForm.js
@@ -1,10 +1,10 @@
 import React from "react";
 function DetailsForm(props) {
   const { id, details } = props;
-  return details.filter((detail) => {
-    {
-      return detail.id === id && <h1> {detail.description} </h1>;
-    }
-  });
+  console.log("props in detail form are",props)
+  // return details.filter((detail) => {
+    return (<h1> {details.text} </h1>)
+    
+  // });
 }
 export default DetailsForm;
